a167e5ae000f7f0bf943ede81fb8523755e5ef60
Fr4nco77 <fcarreras777@gmail.com>
2025-08-08 23:56:20 -0300

    new icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -3,7 +3,7 @@ export const Play = () => {
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="aspect-square w-full"
+      className="aspect-square h-full"
     >
       <path
         fillRule="evenodd"
@@ -19,7 +19,7 @@ export const Pause = () => {
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="aspect-square w-full"
+      className="aspect-square h-full"
     >
       <path
         fillRule="evenodd"
@@ -35,7 +35,7 @@ export const VolumeOn = () => {
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="aspect-square w-full"
+      className="aspect-square h-full"
     >
       <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
       <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
@@ -48,9 +48,41 @@ export const VolumeOff = () => {
     <svg
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="aspect-square w-full"
+      className="aspect-square h-full"
     >
       <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM17.78 9.22a.75.75 0 1 0-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L20.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z" />
+    </svg>
+  );
+};
+
+export const Info = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="aspect-square h-full"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export const Spinner = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="aspect-square h-full animate-spin transition-transform duration-500"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+        clipRule="evenodd"
+      />
     </svg>
   );
 };
