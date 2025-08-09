7dfab38406260af232965bc2186a077531349016
Fr4nco77 <fcarreras777@gmail.com>
2025-08-09 00:01:49 -0300

    now set podcast

--- src/components/player/PlayPauseButton.tsx
+++ src/components/player/PlayPauseButton.tsx
@@ -1,26 +1,42 @@
-import { Pause, Play } from "@assets/iconsPlayer";
+import { Pause, Play } from "@assets/icons";
 import type { ButtonHTMLAttributes } from "react";
 import { usePlayerStore } from "src/store/playerStore";
 
+interface PlayPauseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  podcast?: {
+    id: string;
+    title: string;
+    image: string;
+    audio: string;
+  };
+}
+
 export default function PlayPauseButton({
+  podcast,
   className,
+  children,
   ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
-  const { currentPodcast, isPlaying, setIsPlaying } = usePlayerStore();
+}: PlayPauseButtonProps) {
+  const { currentPodcast, isPlaying, setIsPlaying, setCurrentPodcast } =
+    usePlayerStore();
 
   const handlePlayPause = () => {
-    if (!currentPodcast) return;
-    setIsPlaying(!isPlaying);
+    if (podcast && currentPodcast?.id !== podcast.id) {
+      setCurrentPodcast(podcast);
+    }
+
+    return setIsPlaying(!isPlaying);
   };
 
   return (
     <button
-      className={className}
+      className={`flex items-center justify-center cursor-pointer gap-2 bg-transparent ${className}`}
       onClick={handlePlayPause}
       aria-label={isPlaying ? "Pausar" : "Reproducir"}
       {...props}
     >
       {isPlaying ? <Pause /> : <Play />}
+      {children}
     </button>
   );
 }
