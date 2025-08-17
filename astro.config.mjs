4ae3b7a002f1d90701ceeda76a6f94ed33359862
Fr4nco77 <fcarreras777@gmail.com>
2025-08-16 23:37:54 -0300

    implement seo and delete netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/episodes": "/episodes/1"
+    "/episodes": "/episodes/1",
   },
-
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
   integrations: [react()],
-  adapter: netlify()
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+        },
+      ],
+    ],
+  },
 });
