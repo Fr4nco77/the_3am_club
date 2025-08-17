d977d6580ac6fea4a37b654f664f6d78acee888a
Fr4nco77 <fcarreras777@gmail.com>
2025-08-17 00:04:35 -0300

    add dynamic sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import rehypeExternalLinks from "rehype-external-links";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://the3amclub.vercel.app',
   redirects: {
     "/episodes": "/episodes/1",
   },
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   markdown: {
     rehypePlugins: [
       [
