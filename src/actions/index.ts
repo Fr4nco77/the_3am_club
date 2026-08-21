ba47316d9b3e3b1dc8f95f2f0f8baa5093bfba18
Franco Carreras <fcarreras777@gmail.com>
2026-08-21 15:36:39 -0300

    feat: enhance contact form handling with provider support and improved toast notifications

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,25 +1,53 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { getSender } from "src/contact";
+import { PROVIDERS } from "src/contact/providers";
+import { successMessage } from "src/contact/utils";
 
 export const server = {
   contactAction: defineAction({
     accept: "form",
     input: z.object({
-      name: z.string().min(1, "El nombre es obligatorio"),
-      email: z.string().email("Email inválido"),
-      message: z.string().min(5, "El mensaje es muy corto"),
+      name: z.string().trim().min(1, "El nombre es obligatorio"),
+      email: z.string().trim().email("Email inválido"),
+      message: z.string().trim().min(5, "El mensaje es muy corto"),
+      provider: z
+        .enum(PROVIDERS, {
+          message:
+            "Error 500: Por favor intente comunicarse con el administrador",
+        })
+        .optional(),
     }),
     handler: async (input) => {
       try {
-        // Aquí iría la lógica de envío (correo, BD, etc.)
+        if (input.provider) {
+          const sender = getSender(input.provider);
+          const { success, error, info } = await sender.send(input);
+
+          if (!success) {
+            console.error("[Contact Action Error]:", error);
+            return {
+              success,
+              message: "Hubo un error al procesar tu solicitud.",
+            };
+          }
+
+          return {
+            success,
+            message: info as string,
+          };
+        }
+
+        // Si no hay provider especificado (modo por defecto)
         return {
           success: true,
-          message: `¡${input.name} tu mensaje ha sido enviado con éxito!`,
+          message: successMessage(input.name),
         };
       } catch (err) {
+        console.error("[Action Server Exception]:", err);
         return {
           success: false,
-          message: "Hubo un error al enviar el mensaje.",
+          message: "Ocurrió un fallo en el servidor.",
         };
       }
     },
